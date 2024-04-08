f4421eb6ef2657b52191fae7c1856969811d151f
reri013 <rerizani.reda@gmail.com>
2024-04-08 06:53:59 +0200

    check

--- flask/flask/app1.py
+++ flask/flask/app1.py
@@ -1,1 +1,48 @@
 # Let's write your code here!
+from flask import Flask , request 
+import requests
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "Hello queenie!"
+
+@app.route("/morning")
+def good_morning():
+    return "Good morning!"
+
+@app.route("/evening/<firstname>")
+def evening(firstname):
+    return f"Good evening, {firstname}!"
+
+@app.route("/greetings/<period_of_day>/<firstname>")
+def greetings(period_of_day, firstname):
+    return f"Good {period_of_day}, {firstname}!"
+
+@app.route("/add/<int:first>/<int:second>")
+def add(first, second):
+    return str(first + second)
+
+@app.route("/afternoon")
+def good_afternoon():
+    firstname = request.args['firstname']
+    return f"Good morning {firstname}!"
+
+@app.route("/afternoon2")
+def good_afternoon2():
+    firstname = request.args.get('firstname', 'you')
+    return f"Good morning, {firstname}!"
+
+@app.route("/substract")
+def difference():
+    first = int(request.args.get('first', '0'))
+    second = int(request.args.get('second', '0'))
+    return str(first - second)
+
+@app.route("/hello")
+def hello_api():
+    return {"message": "Hello!", "hey": "I'm an API!"}
--- flask/flask/app2.py
+++ flask/flask/app2.py
@@ -1,1 +1,111 @@
-# Let's write your code here!
+import json
+import math
+from collections import defaultdict
+from flask import Flask, abort, request
+from flask_basicauth import BasicAuth
+from flask_swagger_ui import get_swaggerui_blueprint
+import pymysql
+from requests.auth import HTTPBasicAuth
+from flask_swagger_ui import get_swaggerui_blueprint
+
+app = Flask(__name__)
+
+
+MAX_PAGE_SIZE = 100
+
+
+@app.route("/movies")
+def movies():
+    # URL parameters
+    page = int(request.args.get('page', 0))
+    page_size = int(request.args.get('page_size', MAX_PAGE_SIZE))
+    page_size = min(page_size, MAX_PAGE_SIZE)
+    include_details = bool(int(request.args.get('include_details', 0)))
+
+    db_conn = pymysql.connect(host="localhost",
+                            user="root", 
+                            database="bechdel",  
+                            password = "-1Xy781227@",
+                            cursorclass=pymysql.cursors.DictCursor)
+    # Get the movies
+    with db_conn.cursor() as cursor:
+        cursor.execute("""
+            SELECT
+                M.movieId,
+                M.originalTitle,
+                M.primaryTitle AS englishTitle,
+                B.rating AS bechdelScore,
+                M.runtimeMinutes,
+                M.startYear AS year,
+                M.movieType,
+                M.isAdult
+            FROM Movies M
+            JOIN Bechdel B ON B.movieId = M.movieId 
+            ORDER BY movieId
+            LIMIT %s
+            OFFSET %s
+        """, (page_size, page * page_size))
+        movies = cursor.fetchall()
+        movie_ids = [mov['movieId'] for mov in movies]
+    
+    if include_details:
+        # Get genres
+        with db_conn.cursor() as cursor:
+            placeholder = ','.join(['%s'] * len(movie_ids))
+            cursor.execute(f"SELECT * FROM MoviesGenres WHERE movieId IN ({placeholder})",
+                        movie_ids)
+            genres = cursor.fetchall()
+        genres_dict = defaultdict(list)
+        for obj in genres:
+            genres_dict[obj['movieId']].append(obj['genre'])
+        
+        # Get people
+        with db_conn.cursor() as cursor:
+            placeholder = ','.join(['%s'] * len(movie_ids))
+            cursor.execute(f"""
+                SELECT
+                    MP.movieId,
+                    P.personId,
+                    P.primaryName AS name,
+                    P.birthYear,
+                    P.deathYear,
+                    MP.category AS role
+                FROM MoviesPeople MP
+                JOIN People P on P.personId = MP.personId
+                WHERE movieId IN ({placeholder})
+            """, movie_ids)
+            people = cursor.fetchall()
+        people_dict = defaultdict(list)
+        for obj in people:
+            movieId = obj['movieId']
+            del obj['movieId']
+            people_dict[movieId].append(obj)
+
+        # Merge genres and people into movies
+        for movie in movies:
+            movieId = movie['movieId']
+            movie['genres'] = genres_dict[movieId]
+            movie['people'] = people_dict[movieId]
+
+    # Get the total movies count
+    with db_conn.cursor() as cursor:
+        cursor.execute("SELECT COUNT(*) AS total FROM Movies")
+        total = cursor.fetchone()
+        last_page = math.ceil(total['total'] / page_size)
+
+    db_conn.close()
+    return {
+        'movies': movies,
+        'next_page': f'/movies?page={page+1}&page_size={page_size}&include_details={int(include_details)}',
+        'last_page': f'/movies?page={last_page}&page_size={page_size}&include_details={int(include_details)}',
+    }
+    
+    
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    base_url='/docs',
+    api_url='/static/openapi.yaml',
+)
+app.register_blueprint(swaggerui_blueprint)
+
+
